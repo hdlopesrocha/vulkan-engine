52500e5670d3eefa587b747f624c051bb71d710c
Henrique Rocha <hdlopesrocha@protonmail.com>
2026-04-08 17:21:36 +0100

    ThreadContext: add invokedCubeCalls set for border deduplication
    Add  (hash set) and robin_set fallback to track shifted cubes handled by  to avoid duplicate calls.

--- space/ThreadContext.hpp
+++ space/ThreadContext.hpp
@@ -11,6 +11,16 @@ namespace tsl {
     using robin_map = std::unordered_map<K, V, H>;
 }
 #endif
+// Prefer tsl::robin_set when available, otherwise fall back to std::unordered_set
+#if __has_include(<tsl/robin_set.h>)
+#include <tsl/robin_set.h>
+#else
+#include <unordered_set>
+namespace tsl {
+    template <typename K, typename H = std::hash<K>>
+    using robin_set = std::unordered_set<K, H>;
+}
+#endif
 #include <shared_mutex>
 #include "../math/BoundingCube.hpp"
 #include "OctreeNodeLevel.hpp"
@@ -19,6 +29,9 @@ class ThreadContext {
 public:
     tsl::robin_map<glm::vec3, float> shapeSdfCache;
     tsl::robin_map<glm::vec4, OctreeNodeLevel> nodeCache;
+    // Tracks which shifted cubes have already been passed to the border handler
+    // to avoid calling the handler multiple times for the same cube+level.
+    tsl::robin_set<glm::vec4> invokedCubeCalls;
     std::shared_mutex mutex;
     BoundingCube cube;
     ThreadContext(BoundingCube cube);
